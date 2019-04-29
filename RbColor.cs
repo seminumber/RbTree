4fe3174ce0d9ada54def7e75205c7c23a48e4dc8
Junsoo Ha <seminumber@gmail.com>
2019-04-29 17:04:02 +0900

    first commit

---
+++ RbColor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RbTreeLibrary
+{
+    public enum RbColor
+    {
+        None = -1, // only for criteria
+        Red = 0,
+        Black = 1,
+        DoubleBlack_Red = 2, // temparary state
+        DoubleBlack = 3 // temporary state
+    }
+}
